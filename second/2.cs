74fb70e1ee1dc38e1997ecea90ce628039a6747e
Diana <kat.lahuro.3223@gmail.com>
2023-02-11 15:32:24 +0300

    2

---
+++ second/2.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+System.Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int N = Convert.ToInt32(number);
+int Sum=0;
+while(N>0)
+{
+  Sum=Sum+N%10;
+  N=N/10;
+}
+System.Console.WriteLine(Sum);
