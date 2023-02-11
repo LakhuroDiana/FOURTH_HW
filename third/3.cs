cece8c71a72bd338cfbf67198cdca654131ba447
Diana <kat.lahuro.3223@gmail.com>
2023-02-11 15:33:14 +0300

    3

---
+++ third/3.cs
@@ -0,0 +1,23 @@
+﻿void input (float[] array, int len)
+{  string number;
+   System.Console.WriteLine("Введите массив");
+
+   for (int i=0; i<len; ++i)
+   {
+     number = Console.ReadLine();
+     array[i] = Convert.ToInt32(number);
+   }
+}
+
+void output (float[] array, int len)
+{
+    for (int i=0; i<len; ++i)
+    {
+        System.Console.Write(array[i]);
+        System.Console.Write(" ");
+    }
+}
+ float[] massive = new float[8];
+ int a=massive.Length;
+ input(massive, a);
+ output(massive, a);
